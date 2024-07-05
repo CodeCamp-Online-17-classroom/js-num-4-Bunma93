fcf8a050cd9963dec6a75430c8a6624527dc615f
Bunma93 <bunma93@gmail.com>
2024-07-05 16:43:03 +0700

    Js-num4

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+function transform() {
+    let num = Number(prompt("กรอกตัวเลข"));
+    let result = num.toFixed(2);
+    return result;
+}
+
+let result = transform();
+alert(result);
